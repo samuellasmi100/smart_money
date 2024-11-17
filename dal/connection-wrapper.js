43e390ab37b21d867578ec624e0e1dcc192b4ce0
slasmi-makor <slasmi@makor-capital.com>
2024-11-17 15:24:28 +0200

    change connection configure

--- dal/connection-wrapper.js
+++ dal/connection-wrapper.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2")
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-    host: process.env.HOST, 
-    user: process.env.USER, 
-    password: process.env.PASSWORD, 
-    database:process.env.NAME 
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD, 
+    database:process.env.DB_NAME 
 })
 
 connection.connect(err => {
